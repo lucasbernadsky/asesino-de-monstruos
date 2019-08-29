b65570d25b29a2c489385691f21079d20e9fa614
Lucas Bernadsky <lucas.bernadsky@gmail.com>
2019-08-28 21:03:27 -0300

    prueba

--- app.js
+++ app.js
@@ -1,41 +1,44 @@
-new vue({
-        el: '#app',
-        data: {
-            vidaJugador = 100,
-            vidaMounstro = 100,
-            eventos: []
-        },
-        computed: {
-            mostrarLog(){
-                for(let i = 0; i < eventos.length; i++){
-                    const entrada = eventos[i];
-                    alert(entrada);
-                }
-            }
+new Vue({
+  el: "#app",
+  data: {
+    vidaJugador: 100,
+    vidaMounstro: 100,
+    eventos: []
+  },
+  computed: {
+    mostrarLog() {
+      for (let i = 0; i < eventos.length; i++) {
+        const entrada = eventos[i];
+        alert(entrada);
+      }
+    }
+  },
+  methods: {
+    atacar() {
+      //El jugador ataca con 1 a 5
+      let ataqueJugador = generarRandom(5);
+      //El mounstro ataca con 1 a 10
+      let ataqueMounstro = generarRandom(10);
 
-        },
-        methods: {
-            atacar() {
-                //El jugador ataca con 1 a 5
-                let ataqueJugador = generarRandom(5);
-                vidaJugador -= ataqueJugador;
-                //El mounstro ataca con 1 a 10
-                let ataqueMounstro = generarRandom(10);
-                vidaMounstro -= ataqueMounstro;
+      vidaJugador -= ataqueMounstro;
+      vidaMounstro -= ataqueJugador;
 
-                alert(vidaJugador + vidaMounstro);
-            },
-            atacarEspecial() {
-                //El jugador ataca con 1 a 8
-                let ataqueJugador = generarRandom(8);
+      alert(vidaJugador + vidaMounstro);
+      console.log(vidaJugador + " " + vidaMounstro);
 
-                //El mounstro ataca con 1 a 10
-                let ataqueMounstro = generarRandom(10);
-            },
-            generarRandom(valorAtaque) {
-                let valor = Math.floor(Math.random() * valorAtaque) + 1;
+      return true;
+    },
+    atacarEspecial() {
+      //El jugador ataca con 1 a 8
+      let ataqueJugador = generarRandom(8);
 
-                return valor;
-            }
-        }
-    })
+      //El mounstro ataca con 1 a 10
+      let ataqueMounstro = generarRandom(10);
+      return true;
+    },
+    generarRandom(valorAtaque) {
+      let valor = Math.floor(Math.random() * valorAtaque) + 1;
+      return valor;
+    }
+  }
+});
